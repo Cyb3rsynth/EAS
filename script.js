df9d74d1b80ec8d01a74f33a5cb30656d2438c96
Cyb3rsynth <acp46800@gmail.com>
2022-08-20 12:46:34 +0800

    Updated function to display random RGB color trail, updated eraser button with function. Added CSS styling to buttons and updated Headers with styling

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const container = document.getElementById("container");
 const smallSize = document.querySelector(".sizebtn1");
 const medSize = document.querySelector(".sizebtn2");
 const largeSize = document.querySelector(".sizebtn3");
+const modergb = document.querySelector(".modergb");
+const modeerase = document.querySelector(".modeerase");
 let cell;
 let target; 
 let gridSize;
@@ -14,7 +16,7 @@ let gridSize;
     container.style.setProperty('--grid-cols', gridSize);
     for (i = 0; i < (gridSize * gridSize); i++) {
         let cell = document.createElement('div');
-        container.appendChild(cell).className = 'grid-cell';
+        container.appendChild(cell).className = 'gridCell';
     }
 }
 
@@ -28,34 +30,21 @@ function removeAllGridCells() {
 
  //functions to change size of the grid
   document.querySelector(".sizebtn1").onclick = () => {
-    makeRows(12, 12); 
+    makeRows(8, 8); 
   }
   document.querySelector(".sizebtn2").onclick = () => {
-     makeRows(24, 24); 
+     makeRows(32, 32); 
   }
   document.querySelector(".sizebtn3").onclick = () => {
-     makeRows(48, 48);
-  }
+    makeRows(64, 64); 
+ }
 
-//Functions to change the pixel color of the grid
-  document.querySelector(".modergb").onclick = () => {
-    function getRandomColor (target) {
-        target.style.backgroundColor = getRandomColor(target);
-    }
-    container.addEventListener("mouseover", function (e) {
-    let container = document.querySelector("container");
-        target = e.target;
-    
-        if (target.matches("div")) {
-            getRandomColor(target);
-        }
-    }); 
-  }
 
 //function to highlight the divs as you move over them with your mouse
 function changeColor (target) {
     target.style.backgroundColor = 'black';
 }
+
 
 container.addEventListener("mouseover", function (e) {
 let container = document.querySelector("container");
@@ -72,10 +61,45 @@ reset.addEventListener('click', function(){
      window.location.reload();
 });
 
-//Get a random color
-function getRandomColor() {
-    const r = Math.floor(Math.random() * 256)
-    const g = Math.floor(Math.random() * 256)
-    const b = Math.floor(Math.random() * 256)
-    return `rgb(${r}, ${g}, ${b})`
-}
+//Function for colored divs
+
+modergb.addEventListener("click", () => {
+    let color = "rainbow";
+    setColor(color);
+  });
+
+modeerase.addEventListener("click", () => {
+    let color = "erase";
+    setColor(color);
+  });
+
+function randomNum(min, max) {
+    const num = Math.floor(Math.random() * (max - min)) + min;
+    return num;
+  }
+  
+function randomColor() {
+    return (
+      "rgb(" +
+      randomNum(0, 255) +
+      ", " +
+      randomNum(0, 255) +
+      ", " +
+      randomNum(0, 255) +
+      ")"
+    );
+  }
+
+function setColor(color) {
+    const container = document.querySelectorAll("#container");
+    for (let i = 0; i < container.length; i++) {
+      container[i].addEventListener("mouseover", function (e) {
+         if (color == "rainbow") {
+          e.target.style.backgroundColor = randomColor();
+         }
+         else {
+            e.target.style.backgroundColor = "rgba(255, 255, 246, 0.877)";
+         }
+      });
+    }
+  }
